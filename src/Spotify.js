a9e0e3b5026d99ffaaa524c97cf2758c5c39d058
Caio Marcellos <caiocuritiba@gmail.com>
2018-02-24 18:12:59 -0300

    changing from justen to my application. justen is working from index_older.html; I will store older files in a bkp folder

---
+++ src/Spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapperTdd from 'spotify-api-wrapper-tdd';
+
+const spotify = new SpotifyWrapperTdd({
+  token: 'BQC2PEmgYdpv9991NlyU5EVqYUPbv6r0xtDaszMotWpuCcUzSUK9baD_7AjQI_eimul-jcX4klvRhS5CAyTxiqimicNo0X10_0z9PQUPnT24c7aoD3ac6GvoRVqZc0hv-M0h0Ytcm62TxXg'
+});
+
+export default spotify;
